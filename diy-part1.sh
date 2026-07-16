4e9eac874f3578cb43be518c70a848642b9a264f
admin-mmm-cpu <9ccyz3a9@minutemail.xyz>
2026-07-16 10:37:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# 仅替换固件下载域名，不改动 feeds 拉取逻辑
+# 修复废弃域名SSL证书不匹配问题
+sed -i 's|https://git.lede-project.org|https://git.openwrt.org|g' feeds.conf.default
+# 同步替换软件下载源归档地址
 sed -i 's|https://downloads.lede-project.org|https://archive.openwrt.org|g' feeds.conf.default
